541b22ff96737a961048559c44358d2852047684
trey.kenyon <56365170+webx0jedi@users.noreply.github.com>
2025-10-31 16:50:23 -0700

    Adding comments for clarity
    Added comments for better readability and understanding

--- createProcess.c
+++ createProcess.c
@@ -3,14 +3,16 @@
 
 int main(void) {
 	
+	//pointers to process information
 	STARTUPINFOA si = { 0 };
 	PROCESS_INFORMATION pi = { 0 };
 
+	//loop that retrives last error if the process could not be created
 	if (!CreateProcessW(L"C:\\Windows\\System32\\notepad.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
 		printf("Error. Failed to create process: %ld", GetLastError());
 		return EXIT_FAILURE;
 	}
-
+	//variables that store the thread and Process IDs with pi pointer
 	DWORD TID = pi.dwThreadId;
 	DWORD PID = pi.dwProcessId;
 	HANDLE hThread = pi.hThread;
@@ -19,10 +21,10 @@ int main(void) {
 	printf("Process started! \n");
 	printf("PID of created process: %ld\n", PID);
 	printf("TID of created thread: %ld\n", TID);
-	printf("Handle of Process: 0x%x\n", hProcess);
+	printf("Handle of Process: 0x%x\n", hProcess); //prints the handles of process and thread in hex form
 	printf("Handle of Thread: 0x%x\n", hThread);
 
-	WaitForSingleObject(hProcess, INFINITE);
+	WaitForSingleObject(hProcess, INFINITE); //waits for process to be closed before exiting
 	printf("Finished process! Exiting...\n");
 
 	CloseHandle(hThread);
